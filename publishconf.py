f363a83e2f1cc6259bc52ccc6f7e5c3b0478b086
Mariotaku <mariotaku.lee@gmail.com>
2021-11-18 15:01:18 +0900

    using relative URL

--- publishconf.py
+++ publishconf.py
@@ -7,8 +7,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = '//webosbrew.org'
-RELATIVE_URLS = False
+RELATIVE_URLS = True
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
